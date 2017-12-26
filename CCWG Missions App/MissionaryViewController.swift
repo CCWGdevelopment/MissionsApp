b53f01a5097064e77a9d8f8173665d88e0a43fcd
mhoupt856 <marcus@ccwg.org>
2017-12-26 15:07:08 -0800

    Added newsletter sign up functionality Added support missionary button functionality

--- CCWG Missions App/MissionaryViewController.swift
+++ CCWG Missions App/MissionaryViewController.swift
@@ -9,6 +9,7 @@
 import UIKit
 import Foundation
 import MessageUI
+import SafariServices
 
 class MissionaryViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
@@ -17,8 +18,10 @@ class MissionaryViewController: UIViewController, MFMailComposeViewControllerDel
     @IBOutlet weak var missionaryImageViewer: UIImageView!
     @IBOutlet weak var missionaryBioLabel: UILabel!
     @IBOutlet weak var sendEmailButton: UIButton!
+    @IBOutlet weak var supportMissionaryButton: UIButton!
     
-    var buttonString:String = "Send an email to \(selectedNameArray[missionaryIndex])"
+    var sendEmailButtonString:String = "Send an email to \(selectedNameArray[missionaryIndex])"
+    var supportButtonString:String = "Support \(selectedNameArray[missionaryIndex])"
     
     
     override func viewDidLoad() {
@@ -28,7 +31,8 @@ class MissionaryViewController: UIViewController, MFMailComposeViewControllerDel
         missionaryTitleImageViewer.image = UIImage(named: selectedImage)
         missionaryImageViewer.image = UIImage(named: selectedImageArray[missionaryIndex])
         missionaryBioLabel.text = selectedLongInfoArray[missionaryIndex]
-        sendEmailButton.setTitle(buttonString, for: UIControlState.normal)
+        sendEmailButton.setTitle(sendEmailButtonString, for: UIControlState.normal)
+        supportMissionaryButton.setTitle(supportButtonString, for: UIControlState.normal)
 
         // Do any additional setup after loading the view.
     }
@@ -43,6 +47,8 @@ class MissionaryViewController: UIViewController, MFMailComposeViewControllerDel
     override var prefersStatusBarHidden: Bool {
         return true
     }
+  
+    // Send email button functions
     
     @IBAction func sendEmail(_ sender: Any) {
         
@@ -67,6 +73,36 @@ class MissionaryViewController: UIViewController, MFMailComposeViewControllerDel
         return mailComposerVC
     }
     
+    // Newsletter sign up functions
+    
+    @IBAction func sendNewsletterEmail(_ sender: Any) {
+        
+        let newsletterComposeViewController = configuredNewsletterMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(newsletterComposeViewController, animated: true, completion: nil)
+        }
+        else {
+            self.showSendMailErrorAlert()
+        }
+        
+    }
+    
+    
+    func configuredNewsletterMailComposeViewController() -> MFMailComposeViewController {
+        let newsletterComposerVC = MFMailComposeViewController()
+        newsletterComposerVC.mailComposeDelegate = self
+        newsletterComposerVC.setToRecipients(["marcus@ccwg.org"])
+        
+        newsletterComposerVC.setSubject("Sign me up for \(selectedNameArray[missionaryIndex])'s newsletter!")
+        newsletterComposerVC.setMessageBody("Please enter your email address here to sign up for newsletter:", isHTML: false)
+        
+        return newsletterComposerVC
+    }
+
+    
+    // email error handling
+    
+    
     func showSendMailErrorAlert() {
         
         let sendMailErrorAlert = UIAlertController(title: "Could not send email", message: "Your device must have an active email account", preferredStyle: UIAlertControllerStyle.alert)
@@ -77,6 +113,12 @@ class MissionaryViewController: UIViewController, MFMailComposeViewControllerDel
     
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
         controller.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func supportMissionary(_ sender: Any) {
+        let url = URL(string: "http://ccwgweb.myshelby.org/default.aspx?page=3412")
+        let safariVC = SFSafariViewController(url: url!)
+        present(safariVC, animated: true, completion: nil)
     }
     
     
