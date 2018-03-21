da16639a6ca97f185e2678bfcc7106bf44cafede
Michael Lavello <michaellavello@michaels-macbook-pro.local>
2018-03-21 13:19:48 -0700

    >= for collection view height.

--- CCWG Missions App/ViewControllers/DetailController.swift
+++ CCWG Missions App/ViewControllers/DetailController.swift
@@ -58,6 +58,62 @@ class DetailController: UIViewController, UITableViewDataSource, UITableViewDele
         return true
     }
     
+     // Bullet Point Formatting, if I am lucky.
+  /*
+    let label = UILabel()
+    
+    
+    
+    
+    func add (selectedCountryDescription: [String],
+        font: UIFont,
+        bullet: String = "\u{2022}",
+        indentation: CGFloat = 20,
+        lineSpacing: CGFloat = 2,
+        paragraphSpacing: CGFloat = 12,
+        textColor: UIColor = .black,
+        bulletColor: UIColor = .red) -> NSAttributedString {
+        
+        let textAttributes: [NSAttributedStringKey: Any] = [NSAttributedStringKey.font: font, NSAttributedStringKey.foregroundColor: textColor]
+        let bulletAttributes: [NSAttributedStringKey: Any] = [NSAttributedStringKey.font: font, NSAttributedStringKey.foregroundColor: bulletColor]
+
+        let paragraphStyle = NSMutableParagraphStyle()
+        let nonOptions = [NSTextTab.OptionKey: Any]()
+        paragraphStyle.tabStops = [
+            NSTextTab(textAlignment: .left, location: indentation, options: nonOptions)]
+        paragraphStyle.defaultTabInterval = indentation
+        //paragraphStyle.firstLineHeadIndent = 0
+        //paragraphStyle.headIndent = 20
+        //paragraphStyle.tailIndent = 1
+        paragraphStyle.lineSpacing = lineSpacing
+        paragraphStyle.paragraphSpacing = paragraphSpacing
+        paragraphStyle.headIndent = indentation
+        
+        let bulletList = NSMutableAttributedString()
+        for string in selectedCountryDescription {
+            let formattedString = "\(bullet)\t\(string)\n"
+            let attributedString = NSMutableAttributedString(string: formattedString)
+            
+            attributedString.addAttributes(
+                [NSAttributedStringKey.paragraphStyle : paragraphStyle],
+                range: NSMakeRange(0, attributedString.length))
+            
+            attributedString.addAttributes(
+                textAttributes,
+                range: NSMakeRange(0, attributedString.length))
+            
+            let string:NSString = NSString(string: formattedString)
+            let rangeForBullet:NSRange = string.range(of: bullet)
+            attributedString.addAttributes(bulletAttributes, range: rangeForBullet)
+            bulletList.append(attributedString)
+        }
+        
+        return bulletList
+    }
+ 
+    */
+    
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         if selectedCountry == "china" {
             return chinaMissionaryName.count
